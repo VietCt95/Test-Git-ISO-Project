effde8edbf3d67caa28a1aca04ed54d205beff80
VietCT <macpro@localhost.(none)>
2014-09-25 16:43:33 -0700

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by VietCT on 9/25/14.
+//  Copyright (c) 2014 FlyingPig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by VietCT on 9/25/14.
+//  Copyright (c) 2014 FlyingPig. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
